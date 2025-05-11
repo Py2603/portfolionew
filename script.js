5257d162721fc1f8109767471a4de12c8ec3e6b2
priyanshu yadav <100601595+Py2603@users.noreply.github.com>
2025-05-11 11:27:11 +0530

    Add files via upload

---
+++ script.js
@@ -0,0 +1,8 @@
+// Load Animation
+var animation = lottie.loadAnimation({
+  container: document.getElementById("animation-container"),
+  renderer: "svg",
+  loop: true,
+  autoplay: true,
+  path: "Animation.json", // Path to your animation file
+});
